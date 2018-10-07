efc161549b1f8d98c79bf2fc9512a9d752596736
tommy wahyu <tommywahyu44@gmail.com>
2018-10-07 20:48:14 +0700

    update2

---
+++ app/src/main/java/com/example/bogi/psymate/ChatbotActivity.java
@@ -0,0 +1,15 @@
+package com.example.bogi.psymate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class ChatbotActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+    }
+}
